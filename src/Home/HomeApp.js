710db31ca64bcfbaf86ad1a78d8a7e1e97a49460
Ionut Daniel Ene <ionut.daniel644@gmail.com>
2023-02-21 12:02:11 +0200

    WIP

--- src/Home/HomeApp.js
+++ src/Home/HomeApp.js
@@ -14,73 +14,15 @@ function HomeApp() {
 
     ScrollTrigger.defaults({ markers: { startColor: "green", endColor: "red" } });
 
-    const component = useRef();
-    const slider = useRef();
-
-    useLayoutEffect(() => {
-        let ctx = gsap.context(() => {
-            gsap.to(".experience", {
-                ease: "none",
-                scrollTrigger: {
-                    trigger: ".experience",
-                    duration: 2,
-                    pin: true,
-                    scrub: 1,
-                    snap: 1,
-                    start: "top 80%",
-                    end: 'top 100%',
-                    markers: true,
-                }
-            });
-
-            // let panels = gsap.utils.toArray(".panel");
-            // gsap.to(panels, {
-            //     xPercent: -100 * (panels.length - 1),
-            //     ease: "none",
-            //     scrollTrigger: {
-            //         trigger: slider.current,
-            //         pin: true,
-            //         scrub: 0,
-            //         snap: 1 / (panels.length - 1),
-            //         end: () => "+=" + slider.current.offsetWidth,
-            //         markers: true,
-            //     }
-            // });
-            // let experienceScreen = gsap.timeline({
-            //     scale: 0.1,
-            //     scrollTrigger: {
-            //         trigger: ".experience",
-            //         scrub: 1,
-            //         pin: true,
-            //         start: "top 70%",
-            //         end: "top 80%",
-            //         markers: true,
-            //     },
-            // });
-
-            // gsap.set('.title-2', { opacity: 0, scale: 0.5, y: 400, transformOrigin: '50% 50%' })
-
-            // experienceScreen.to('.title-2', { duration: 5, opacity: 1, scale: 1, y: 0 })
-        }, component);
-
-
-
-        return () => ctx.revert();
-    });
 
     return (
         <div className="bg-darkNotDark ">
             <Bg2ColorsFollow />
 
-            <div className="App scroll-smooth " ref={component}>
+            <div className="App scroll-smooth ">
                 <AboutMe />
-                <div ref={slider} className="z-10 containergsap">
-                    <div className="flex items-start justify-center pt-32 panel experience">
-                        <div className="text-white title-2 text-9xl">Work Experience</div>
-
-                    </div>
-                    {/* <div className="text-white panel">TWO</div>
-                    <div className="text-white panel">THREE</div> */}
+                <div className="z-10 containergsap">
+                    <Experience />
                 </div>
                 {/* <div className="lastContainer">Last Container</div> */}
             </div>
@@ -88,14 +30,13 @@ function HomeApp() {
     );
 }
 
+
 // eslint-disable-next-line no-lone-blocks
-{
-    /* <nav className="z-50 flex gap-20">
-                <NavLink to={"/"}>Home</NavLink>
-                <NavLink to={"/Projects/FromDesignToCode/Indonesia"}>
-                    Indonesia
-                </NavLink>
-                <NavLink to={"/Projects/FromDesignToCode/Cards"}>Cards</NavLink>
-                <NavLink to={"/Projects/MazdaRx8"}>MazdaRx8</NavLink>
-            </nav> */
-}
+// /* <nav className="z-50 flex gap-20">
+//             <NavLink to={"/"}>Home</NavLink>
+//             <NavLink to={"/Projects/FromDesignToCode/Indonesia"}>
+//                 Indonesia
+//             </NavLink>
+//             <NavLink to={"/Projects/FromDesignToCode/Cards"}>Cards</NavLink>
+//             <NavLink to={"/Projects/MazdaRx8"}>MazdaRx8</NavLink>
+//         </nav> */
--- src/Home/Screens/Experience.js
+++ src/Home/Screens/Experience.js
@@ -1,9 +1,44 @@
-import React from "react";
+import gsap from "gsap";
+import React, { useEffect } from "react";
 
 function Experience() {
+
+    useEffect(() => {
+
+        gsap.to('.experienceTitle', {
+            ease: "power4.inOut",
+            clipPath: 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)',
+            opacity: 1,
+            y: 0,
+            duration: 2.2,
+            scrollTrigger: {
+                trigger: ".experience",
+                markers: true,
+                scrub: 3,
+                start: "top 60%",
+                end: "top 70%",
+            }
+        })
+        gsap.to(".title1", {
+            ease: "power4.inOut",
+            clipPath: 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)',
+            opacity: 1,
+            y: 0,
+            duration: 2.2,
+            scrollTrigger: {
+                trigger: '.experienceTitle',
+                start: "top 70%",
+                markers: true,
+                id: '2'
+            }
+        })
+    })
     return (
-        <div className="z-50 w-screen h-screen text-white" id="experience">
-            Experience
+        <div className="flex flex-col items-center justify-start w-screen h-screen pt-20 experience">
+            <div className="z-10 text-6xl text-white border-2 experienceTitle">Work Experience</div>
+            <div className="z-10 w-2/6 pt-10 text-2xl text-white title1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
         </div>
     );
 }
