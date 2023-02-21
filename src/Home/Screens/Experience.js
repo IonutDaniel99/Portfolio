127e5d2f516edfba947d7cd2ab12cf23e506cfce
Ionut Daniel Ene <ionut.daniel644@gmail.com>
2023-02-21 12:23:39 +0200

    Some Fix

--- src/Home/Screens/Experience.js
+++ src/Home/Screens/Experience.js
@@ -27,16 +27,16 @@ function Experience() {
             duration: 2.2,
             scrollTrigger: {
                 trigger: '.experienceTitle',
-                start: "top 70%",
-                end: "top 80%",
+                start: "top 50%",
+                end: "top 70%",
                 markers: true,
                 scrub: 3,
             }
         })
     })
     return (
-        <div className="flex flex-col items-center justify-start w-screen h-screen pt-20 experience">
-            <div className="z-10 text-6xl text-white border-2 experienceTitle">Work Experience</div>
+        <div className="flex flex-col items-center justify-start w-screen h-screen experience">
+            <div className="z-10 text-6xl text-white experienceTitle">Work Experience</div>
             <div className="z-10 w-2/6 pt-10 text-2xl text-white title1">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>
