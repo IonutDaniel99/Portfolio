2ece6d69a96b4c076ce821812023744b8e044c91
Ionut Daniel <42089391+IonutDaniel99@users.noreply.github.com>
2023-05-17 19:59:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import CardsApp from './Projects/FromDesignToCode/Cards/CardsApp'
 import IndonesiaApp from './Projects/FromDesignToCode/Indonesia/IndonesiaApp'
 import MazdaRx8Main from './Projects/MazdaRx8/MazdaRx8Main'
 import Landscape3D from './Projects/Landscape3D/Landscape3D'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomeApp />}></Route>
-        <Route path='/Projects/FromDesignToCode/Indonesia' element={<IndonesiaApp />}></Route>
-        <Route path='/Projects/FromDesignToCode/Cards' element={<CardsApp />}></Route>
-        <Route path='/Projects/MazdaRx8' element={<MazdaRx8Main />}></Route>
-        <Route path='/Projects/3DLandscape' element={<Landscape3D />}></Route>
-      </Routes>
-      {/* <BackToPortfolio /> */}
-    </BrowserRouter>
+    <>
+    <Analytics />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomeApp />}></Route>
+          <Route path='/Projects/FromDesignToCode/Indonesia' element={<IndonesiaApp />}></Route>
+          <Route path='/Projects/FromDesignToCode/Cards' element={<CardsApp />}></Route>
+          <Route path='/Projects/MazdaRx8' element={<MazdaRx8Main />}></Route>
+          <Route path='/Projects/3DLandscape' element={<Landscape3D />}></Route>
+        </Routes>
+        {/* <BackToPortfolio /> */}
+      </BrowserRouter>
+  </>
   )
 }
 
