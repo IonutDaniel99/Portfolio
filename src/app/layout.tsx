6461a025216e6f313c4c17e412f4282972c09bab
Daniel Ene <ionut.daniel644@gmail.com>
2025-01-12 20:46:30 +0200

    vercel insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono, Open_Sans } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers'
 import FollowBackground from '@/components/FollowBackground'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const openSans = Open_Sans({
     weight: '500',
@@ -27,6 +29,8 @@ export default function RootLayout({
                 <Providers>
                     <FollowBackground />
                     {children}
+                    <Analytics />
+                    <SpeedInsights />
                 </Providers>
             </body>
         </html>
